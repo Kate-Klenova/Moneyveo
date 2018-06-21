ad6e31c723aab62a4583f3fddb671e0937f45567
Klenova Kateryna <kateryna.klenova@evolvice.de>
2018-06-21 17:45:22 +0300

    1st commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestTask
+{
+    class Moneyveo
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
---
+++ ConsoleApp2/Tests/GoogleSmth_Test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestTask
+{
+    class GoogleSmth_Test
+    {
+        private string _browserName;
+        private IWebDriver _driver;
+        private GooglePage googlePage;
+
+        private string desiredInfo = "Selenium IDE export to C#";
+
+        public GoogleSmth_Test()
+        {
+            googlePage = new GooglePage();
+        }
+
+
+        [SetUp]
+        public void Initialize()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [Test]
+        [TestCase]
+        public void CheckGoogleSearchResultsContainText()
+        {
+            var result = googlePage.GoogleFor(desiredInfo);
+            Assert.True(result.Contains("Selenium IDE"));
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            _driver.Close();
+        }
+    }
+}
